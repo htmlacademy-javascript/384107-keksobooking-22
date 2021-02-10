ebafb04d56ae41674c27eb409fb6300ee58d906e
Feizerr <anastasiapred@gmail.com>
2021-02-10 14:04:53 +0300

    Создает массив из 10 рекламных объявлений

--- js/main.js
+++ js/main.js
@@ -1,37 +1,103 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно
-const getNumbers = (min, max) => {
+const typeOfBuilding = ['palace', 'flat', 'house', 'bungalow'];
+const time = ['12:00', '13:00', '14:00'];
+const featuresType = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const photosLinksArray = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+
+const getRandomNumber = (min, max) => {
+  if (min < 0 || max < 0) {
+    throw Error('Ошибка. Пожалуйста, введите число от 0 и больше');
+  }
+
   if (min >= max) {
-    return 'Ошибка. Пожалуйста, введите другое значение. Минимальное число должно быть меньше максимального';
+    throw Error('Ошибка. Пожалуйста, введите другое значение. Минимальное число должно быть меньше максимального');
   }
 
-  else {
-    if (min >= 0 && max >= 0) {
-      return Math.trunc(Math.random() * (max - min + 1) + min);
-    }
+  return Math.trunc(Math.random() * (max - min + 1) + min);
+}
 
-    return'Ошибка. Пожалуйста, введите число от 0 и больше';
+const getRandomFloatNumber = (min, max, floatLength) => {
+  if (min < 0 || max < 0) {
+    throw Error('Ошибка. Пожалуйста, введите число от 0 и больше');
+  }
+
+  if (min >= max) {
+    throw Error('Ошибка. Пожалуйста, введите другое значение. Минимальное число должно быть меньше максимального');
+  }
+
+  return Number((Math.random() * (max - min) + min).toFixed(floatLength));
+}
+getRandomFloatNumber();
+
+
+const chooseRandomElement = (items) => {
+  return items[Math.floor(Math.random() * items.length)];
+}
+
+const getRandomAvatarValue = () => {
+  const randomAvatarValue = '0' + getRandomNumber(1, 8);
+  return 'img/avatars/user' + randomAvatarValue + '.png';
+}
+
+const createRandomArray = (array) => {
+  const quantityElements = getRandomNumber(0, array.length - 1);
+  let newArray = [];
+
+  for (let i = 0; i <= quantityElements; i++) {
+    let getRandomArrayElement = array[getRandomNumber(0, array.length - 1)];
+    newArray.push(getRandomArrayElement);
+  }
+
+  return newArray;
+}
+
+// const createLocationObject = () => {
+//   return {
+//     x: getRandomFloatNumber(35.65000, 35.70000, 5),
+//     y: getRandomFloatNumber(139.70000, 139.80000, 5),
+//   }
+// }
+// const location = createLocationObject();
+
+const createAdvertismentObject = () => {
+  let locationX = getRandomFloatNumber(35.65000, 35.70000, 5);
+  let locationY = getRandomFloatNumber(139.70000, 139.80000, 5);
+
+  return {
+    author: {
+      avatar: getRandomAvatarValue(1, 8),
+    },
+
+    offer: {
+      title: 'Лучшее жилье только у нас!',
+      address: locationX + ',' + locationY,
+      price: getRandomNumber(1000, 100000),
+      type: chooseRandomElement(typeOfBuilding),
+      rooms: getRandomNumber(1, 5),
+      guests: getRandomNumber(1, 4),
+      checking: chooseRandomElement(time),
+      checkout: chooseRandomElement(time),
+      features: createRandomArray(featuresType),
+      description: 'Удобное и просторное жилье для гостей из разных стран',
+      photos: createRandomArray(photosLinksArray),
+    },
+
+    location: {
+      x: locationX,
+      y: locationY,
+    },
   }
 }
 
-getNumbers(2, 8);
+const creareAdvertismentArray = () => {
+  let advertismentArray = [];
 
-
-// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-
-
-const getCoordinates = (firstCoordinate, secondCoordinate, lengthCoorinates) => {
-
-  if (firstCoordinate >= secondCoordinate) {
-    return 'Ошибка. Пожалуйста, введите другое значение. Минимальное число должно быть меньше максимального';
+  for (let i = 0; i < 10; i++) {
+    const newAdvertismentObject = createAdvertismentObject();
+    advertismentArray.push(newAdvertismentObject);
   }
 
-  else {
-    if (firstCoordinate >= 0 && secondCoordinate >= 0) {
-      return Number((Math.random() * (secondCoordinate - firstCoordinate + 1) + firstCoordinate).toFixed(lengthCoorinates));
-    }
-
-    return 'Ошибка. Пожалуйста, введите число от 0 и больше';
-  }
+  return advertismentArray;
 }
 
-getCoordinates(0.8, 63, 2);
+creareAdvertismentArray();
